8b6d962dff4140813893e5bbf065c386d13c92c8
simonXu <xxh_nus@yahoo.com>
2024-11-23 23:33:59 +0800

    add code to new release branch to combine all mgt and data module into one

--- dataload/src/main/java/com/wang/dataload/data/DataProcessor.java
+++
@@ -1,9 +0,0 @@
-package com.wang.dataload.data;
-
-import org.apache.poi.ss.usermodel.Cell;
-
-public interface DataProcessor {
-
-    public void processData(Cell cell);
-
-}
--- dataloadMongo/src/main/java/com/wang/mongodataload/repository/ProformaInvoiceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wang.mongodataload.repository;
-
-import com.wang.mongodataload.dto.ProformaInvoiceDTO;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProformaInvoiceRepository extends MongoRepository<ProformaInvoiceDTO,Integer> {
-}
